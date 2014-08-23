7dba665918dc1f7aabb82a39cc9061effa7fa2bf
numa08 <n511287@gmail.com>
2014-08-23 17:06:53 +0900

    add comment

--- hackahackersbar.js
+++ hackahackersbar.js
@@ -1,4 +1,5 @@
-var actualCode = "jQuery._data(document.getElementById('gachaBtn01'), 'events').click[0].handler = function () { moveBg(); };";
+//gachaBtn01
+var actualCode = "jQuery._data(document.getElementById('gachaBtn01'), 'events').click[0].handle = function() { moveBg(); };";
 var script = document.createElement('script');
 script.textContent = actualCode;
 (document.head || document.documentElement).appendChild(script);
